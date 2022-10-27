70f8ba984f04b875c88de7a0bfc8029df36dcb3b
Alessandro Bonomo <75626033+AlessandroBonomo28@users.noreply.github.com>
2022-10-27 12:23:51 +0200

    testing doppio for

--- pong.asm
+++ pong.asm
@@ -16,19 +16,38 @@ ENDM
 
 DrawRect MACRO x, y, width, height, color
         
-        mov bh,width
-        sub bh,x  ; tot pixel x
-        mov bl,x,height
-        sub bl,y  ; tot pixel y
+        ;mov bh,width
+        ;sub bh,x  ; tot pixel x
+        ;mov bl,x,height
+        ;sub bl,y  ; tot pixel y
         
-        mov ax,1 ;init counter ax
-loop1   mov cx,bh ; iterazioni primo loop init a tot pixel x   
-loop2   nop ; set pixel TODO
-                 
+         mov dx,0
         
-        loop loop2           
-        cmp ax,bl  ;iterazioni sub-loop int a tot pixel y 
-        je loop1    
+         mov ax,0 ;init counter ax 
+loop1:   mov cx,0  
+loop2:   inc cx  
+ 
+         push ax
+         push cx ; conserva i counter nello stack
+         
+         
+         mov dx,height
+         
+         mul dx ; ax = ax * dx  
+         mov dx,ax
+         add dx,cx ; dx contiene il numero tot di iterazioni
+         
+         nop ; operazioni a piacere for annidato
+         
+         pop cx
+         pop ax ; ripristina i counter
+         
+         ; fine op for annidato        
+         cmp cx,4  ; iterazioni for annidato
+         jne loop2 
+         inc ax          
+         cmp ax,4  ; iterazioni primo for 
+         jne loop1    
         
  
     
@@ -50,16 +69,16 @@ CreateWindow MACRO ; graphical mode. 40x25. 256 colors. 320x200 pixels. 1 page.
 ENDM
 
 org 100h
-        CreateWindow
+        ;CreateWindow
         
         ;0Fch = white
         
-        WriteChar 5,5,49,0Fch
-        WriteChar 5,6,50,03ch
+        ;WriteChar 5,5,49,0Fch
+        ;WriteChar 5,6,50,03ch
         
         
-        ;LEA BX,wBall
-        ;DrawRect 10,10,[BX],[BX],0Fch
+        LEA BX,wBall
+        DrawRect 0,0,4,4,0Fch
         
 loop:   MOV AH,01h
         INT 16H  ; interrupt check input

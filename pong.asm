6d2974b8aa121e33b57e9b5d67e961f5d1ff2fa7
Alessandro Bonomo <75626033+AlessandroBonomo28@users.noreply.github.com>
2022-10-27 14:24:45 +0200

    fix: drawrect

--- pong.asm
+++ pong.asm
@@ -16,37 +16,50 @@ ENDM
 
 DrawRect MACRO x, y, width, height, color
         
-        ;mov bh,width
-        ;sub bh,x  ; tot pixel x
-        ;mov bl,x,height
-        ;sub bl,y  ; tot pixel y
+            
         
          mov dx,0
         
          mov ax,0 ;init counter ax 
 loop1:   mov cx,0  
-loop2:   inc cx  
- 
-         push ax
+loop2:   push ax
          push cx ; conserva i counter nello stack
          
          
-         mov dx,height
          
-         mul dx ; ax = ax * dx  
-         mov dx,ax
-         add dx,cx ; dx contiene il numero tot di iterazioni
          
          nop ; operazioni a piacere for annidato
          
+          ; set graphics video mode. 
+    	  
+    	 pop cx ; cx = ax counter (yoff)
+    	 pop ax ; ax = cx counter = xoff
+    	 
+    	 mov bx,cx ; bx = cx (ax counter)
+    	 
+    	 add cx,y    ; cx = y of pixel for int10h
+    	 
+    	 mov dx,ax
+    	 
+    	 add dx, x   ; dx = x of pixel for int10h 
+    	 
+    	 push ax
+    	 push bx
+    	 
+    	 mov ah, 0ch
+    	 mov al, color
+    	 int 10h     ; set pixel
+         
+         
+         
          pop cx
          pop ax ; ripristina i counter
-         
+         inc cx
          ; fine op for annidato        
-         cmp cx,4  ; iterazioni for annidato
+         cmp cx,width  ; iterazioni for annidato
          jne loop2 
          inc ax          
-         cmp ax,4  ; iterazioni primo for 
+         cmp ax,height  ; iterazioni primo for 
          jne loop1    
         
  
@@ -69,7 +82,7 @@ CreateWindow MACRO ; graphical mode. 40x25. 256 colors. 320x200 pixels. 1 page.
 ENDM
 
 org 100h
-        ;CreateWindow
+        CreateWindow
         
         ;0Fch = white
         
@@ -77,8 +90,11 @@ org 100h
         ;WriteChar 5,6,50,03ch
         
         
-        LEA BX,wBall
-        DrawRect 0,0,4,4,0Fch
+        ;LEA BX,wBall
+        DrawRect 5,5,50,50,1100b 
+        
+        ;DrawRect 0,0,5,5,1100b
+        
         
 loop:   MOV AH,01h
         INT 16H  ; interrupt check input
